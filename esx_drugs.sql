c3112b247c1d5a236bc88cef66999c70d5ea6cfb
Diorges Rocha <diorges@gis.net.br>
2019-12-10 14:34:37 -0400

    Added SQL to insert the drugs into items...

---
+++ esx_drugs.sql
@@ -0,0 +1,10 @@
+INSERT INTO `items` (name, label, `limit`) VALUES
+	('weed', 'Weed (1G)', 420),
+	('weed_pooch', 'Bag of weed (28G)', 15),
+	('coke', 'Coke (1G)', 420),
+	('coke_pooch', 'Bag of coke (28G)', 15),
+	('meth', 'Meth (1G)', 420),
+	('meth_pooch', 'Bag of meth (28G)', 15),
+	('opium', 'Opium (1G)', 420),
+	('opium_pooch', 'Bag of opium (28G)', 15)
+;
